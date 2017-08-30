37a11c8ff8c65f8547891538002ee4bd3f5bd058
Demian Espinosa Ruiz <demianespinosar@gmail.com>
2017-08-30 15:18:58 -0500

    Adding Binary Search Template

---
+++ BinarySearch.cpp
@@ -0,0 +1,15 @@
+// Given a boolean property P defined in 1, 2, ..., N
+// such that there exists an integer x in [1, N]
+// that P(y) = true if and only if x <= y
+// find x  
+
+lo = 1; // The minimum value of x
+hi = N; // The maximum value of x
+while (lo != hi) {
+	mid = (lo + hi)/2;
+	if (P(y))
+		hi = mid;
+	else
+		lo = x + 1;
+}
+return lo;
